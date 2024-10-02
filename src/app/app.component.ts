8e97f480161a0a425031226dcfaf6a52d490b55d
Paul Cousineau <oolotronic@hotmail.com>
2024-10-02 15:43:27 -0700

    Setup Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'test-app';
+
+
+  timeline: any = [
+    {
+      company: 'Plume',
+      start: '2018',
+      end: '2024'
+
+    },
+    {
+      company: 'AgileOne',
+      start: '2016',
+      end: '2018'
+    },
+    {
+      company: 'Badgeville',
+      start: '2015',
+      end: '2016'
+    }
+  ]
+
+
 }
