b1e9a7d615098d4410345b196a9976239b8527fc
Paul Cousineau <oolotronic@hotmail.com>
2024-10-04 19:50:16 -0700

    Email Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { CommonModule  } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import {TopbarComponent} from "./topbar/topbar.component";
 import {TimelineComponent} from "./timeline/timeline.component";
 import {SkillsComponent} from "./skills/skills.component";
@@ -12,7 +14,8 @@ import {screenName} from "./app.types";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, TopbarComponent, TimelineComponent, SkillsComponent, ResumeComponent, EmailComponent],
+  imports: [RouterOutlet, CommonModule, ReactiveFormsModule,
+    TopbarComponent, TimelineComponent, SkillsComponent, ResumeComponent, EmailComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
